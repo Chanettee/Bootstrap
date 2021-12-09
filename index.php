ef2508b7812708b467e38794c3a19177991f4bd0
Chanettee Leklee <86229643+Chanettee@users.noreply.github.com>
2021-12-09 14:01:06 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>63116503</title>
+
+
+    <div class="container">
+        <div>
+            <img src=" https://f.ptcdn.info/214/050/000/ononbjjl5RFheSJGJpq-o.jpg" class="img-fluid" alt="...">
+        </div>
+
+        <style>
+            .fheader{
+                color: black;
+                text-align:center;
+            }
+        </style>
+        
+        <div class="fheader">
+            <h1>K-pop</h1>
+            <p>GOT7</p>
+        </div>
+
+</head>
+
+<br>
+
+<body>
+    <div class="row">
+        <div class="col-4">
+            <img src="https://i.pinimg.com/564x/3b/ed/9c/3bed9ca0f4bd019c46a1a9d9c82629ad.jpg" class="img-fluid"
+                alt="Mark">
+        </div>
+
+        <div class="col-4">
+            <img src="https://i.pinimg.com/564x/3b/ed/9c/3bed9ca0f4bd019c46a1a9d9c82629ad.jpg" class="img-fluid"
+                alt="Mark">
+        </div>
+
+        <div class="col-4">
+            <img src="https://i.pinimg.com/564x/3b/ed/9c/3bed9ca0f4bd019c46a1a9d9c82629ad.jpg" class="img-fluid"
+                alt="Mark">
+        </div>
+
+
+
+        <div class="row">
+            <div class="col-4">
+                มาร์ก ต้วน เกิดวันที่ 4 กันยายน ค.ศ. 1993 เป็นนักร้อง นักเต้น นักแต่งเพลง สัญชาติอเมริกัน-ไต้หวัน
+                เป็นหนึ่งในสมาชิกวงก็อตเซเวน ประเทศเกาหลีใต้ เริ่มเข้าสู่วงการบันเทิงตั้งแต่ พ.ศ. 2557
+                มีความสามารถพิเศษด้านศิลปะการต่อสู้เป็นบุตรคนที่สาม มีพี่สาว 2 คนและน้องชายอีก 1 คน
+            </div>
+
+            <div class="col-4">
+                ตอนที่เขายังเด็กไม่ได้สนใจที่จะเป็นนักร้อง แต่ในปี ค.ศ. 2010
+                ทางค่ายเจวายพีได้เข้ามารับสมัครออดิชั่นเพื่อเป็นศิลปินที่โรงเรียนของเขา มาร์กไม่ได้สนใจ
+                จนกระทั่งได้รับการผลักดันจากพ่อแม่และทางโรงเรียนบอกให้เขาเข้ารับการคัดเลือก
+                มาร์กจึงตัดสินใจเข้าร่วมจนได้รับคัดเลือก ซึ่งเป็นครั้งแรกที่เขาได้ขึ้นแสดงบนเวที
+            </div>
+
+            <div class="col-4">
+                การฝึกอบรมของเขาใช้เวลาประมาณ 3 ปีครึ่งก่อนที่จะรวมกับสมาชิกอีก 6 คน
+                เปิดตัวอย่างเป็นทางการครั้งแรกเมื่อวันที่ 15 มกราคม ค.ศ. 2014 ในฐานะสมาชิกของวงบอยแบนด์ชาวเกาหลีใต้
+                ก็อตเซเวน มาร์กรับหน้าที่เป็นแร็ปเปอร์
+            </div>
+        </div>
+</body>
+
+<footer>
+    <table class="table table-hover table-bordered border-primary">
+        <caption>รายการโทรทัศน์</caption>
+
+        <thead class="table-dark">
+            <tr>
+                <th>ปี</th>
+                <th>รายการ</th>
+                <th>เครือข่าย</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>2556</td>
+                <td>2558</td>
+                <td>2560</td>
+            </tr>
+            <tr>
+                <td>Who Is Next (WIN)</td>
+                <td>Dream Knight</td>
+                <td>I Can See Your Voice(Original Version)</td>
+            </tr>
+            <tr>
+                <td>เอ็มเน็ตทีวี</td>
+                <td>ทีวีเอ็น</td>
+                <td>Workpoint 23</td>
+            </tr>
+        </tbody>
+    </table>
+
+</footer>
+</div>
+
+</html>
